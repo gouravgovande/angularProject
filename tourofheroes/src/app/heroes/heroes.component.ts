bb3c941eac48c166616fe112dc5b63cf7365e019
gourav.govande <gourav.govande@wisdmlabs.com>
2019-09-12 20:38:26 +0530

    creted heroes component and listed the heroes down also added the click event on hero click

--- tourofheroes/src/app/heroes/heroes.component.ts
+++ tourofheroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero";
+import { HEROES } from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
@@ -14,8 +15,11 @@ export class HeroesComponent implements OnInit {
     
   }
   
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+
+  selectedHero: Hero;
+  
+  onSelect(hero:Hero): void {
+    this.selectedHero = hero;
+  }
 }
